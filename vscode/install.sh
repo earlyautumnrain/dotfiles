43a68b5b968450641eaee39bcc49db386bd68239
Amir Salakhov <amir.salakhov@gmail.com>
2022-02-03 12:43:37 +0300

    add dotbot-brewfile

---
+++ vscode/install.sh
@@ -0,0 +1,8 @@
+code --install-extension tuttieee.emacs-mcx@0.39.0
+code --install-extension alexdima.copy-relative-path@0.0.2
+code --install-extension alexkrechik.cucumberautocomplete@2.15.1
+code --install-extension EditorConfig.EditorConfig@0.16.4
+code --install-extension mikestead.dotenv@1.0.1
+code --install-extension ms-python.python@2021.9.1191016588
+code --install-extension teabyii.ayu@0.20.2
+code --install-extension ms-vscode.Theme-TomorrowKit@0.1.4
