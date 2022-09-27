1c5476088747580871bc1a20de3ce127af010ae8
Amir Salakhov <amir.salakhov@gmail.com>
2022-09-27 12:17:07 +0300

    fix: remove poetry related stuff

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,8 +1,6 @@
-code --install-extension tuttieee.emacs-mcx@0.39.0
+code --install-extension tuttieee.emacs-mcx@0.44.1
 code --install-extension alexdima.copy-relative-path@0.0.2
-code --install-extension alexkrechik.cucumberautocomplete@2.15.1
 code --install-extension EditorConfig.EditorConfig@0.16.4
 code --install-extension mikestead.dotenv@1.0.1
-code --install-extension ms-python.python@2021.9.1191016588
-code --install-extension teabyii.ayu@0.20.2
-code --install-extension ms-vscode.Theme-TomorrowKit@0.1.4
+code --install-extension ms-python.python@2022.15.12631011
+code --install-extension teabyii.ayu@1.0.5
