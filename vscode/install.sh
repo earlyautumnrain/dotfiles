7439d62271c5482094bfbba14b2a40acbc601483
Amir Salakhov <amir.salakhov@gmail.com>
2023-05-18 11:04:13 +0400

    update deps

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,6 +1,8 @@
-code --install-extension tuttieee.emacs-mcx@0.44.1
-code --install-extension alexdima.copy-relative-path@0.0.2
-code --install-extension EditorConfig.EditorConfig@0.16.4
-code --install-extension mikestead.dotenv@1.0.1
-code --install-extension ms-python.python@2022.15.12631011
-code --install-extension teabyii.ayu@1.0.5
+code --install-extension EditorConfig.EditorConfig
+code --install-extension alexdima.copy-relative-path
+code --install-extension chintans98.markdown-jira
+code --install-extension golang.Go
+code --install-extension mathiasfrohlich.Kotlin
+code --install-extension mikestead.dotenv
+code --install-extension ms-python.python
+code --install-extension teabyii.ayu
